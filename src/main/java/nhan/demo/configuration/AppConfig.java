7d8e4e7d35ab03efdb17ee8752bf266a62763488
NhanLocal <nhannhd1004@gmail.com>
2026-05-22 15:51:03 +0700

    CORS

---
+++ src/main/java/nhan/demo/configuration/AppConfig.java
@@ -0,0 +1,81 @@
+package nhan.demo.configuration;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.filter.CorsFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+//@Component
+//public class AppConfig  extends OncePerRequestFilter {
+//
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+//        response.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+//        filterChain.doFilter(request, response);
+//
+//    }
+}
+// CorsFilter
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedHeaders(List.of("*"));
+        // neu muon chi co user thif la /user/**
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+}
+
+// Cors -> use webmvcconfigurer
+//
+////    @Bean
+////    public WebMvcConfigurer corsConfigurer() {
+////        return new WebMvcConfigurer() {
+////            @Override
+////            public void addCorsMappings(CorsRegistry registry) {
+////                registry.addMapping("/**")
+////                    .allowCredentials(true)
+////                    .allowedOrigins("http://localhost:5173")
+////                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+////                    .allowedHeaders("*")    ;
+////            }
+////        }
+////    }
+//
+//
+//    //use WebMvcCònigurer
+////public class AppConfig implements WebMvcConfigurer {
+//
+//
+////    @Override
+////    public void addCorsMappings(CorsRegistry registry) {
+////        registry.addMapping("/**")
+////            .allowCredentials(true)
+////            .allowedOrigins("http://localhost:5173")
+////            .allowedMethods("GET", "POST", "PUT", "DELETE")
+////            .allowedHeaders("*")    ;
+////    }
+//}
